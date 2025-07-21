0b8a535b845e5f5ebd884ce8637e594b6668f652
Benetoro <vicenterpineda19@gmail.com>
2025-07-21 13:42:17 +0200

    Time: 407 ms (62.52%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-SELECT DISTINCT author_id as id
+SELECT author_id as id
 FROM Views
-WHERE author_id = viewer_id
-ORDER BY id
+#filter by author viewing his own article
+WHERE viewer_id = author_id
+#grouping in order to avoid duplicates
+GROUP BY author_id
+#Ordering by id in ASC order
+ORDER BY id ASC
